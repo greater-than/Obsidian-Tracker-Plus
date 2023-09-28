2af9104309e26cbb7e205629ddfd0b979d0525cd
Andrew Benson <andrew.e.benson@gmail.com>
2023-09-28 06:06:33 -0600

    Updates min obsidian version

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  env: {
+    node: true,
+    jest: true,
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  root: true,
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs,ts}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 0,
+    '@typescript-eslint/prefer-nullish-coalescing': 0,
+    '@typescript-eslint/strict-boolean-expressions': 0,
+  },
+};
